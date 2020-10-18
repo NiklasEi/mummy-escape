10a0cec76375a95417bbccce4b263c6e601bb7aa
Jennifer Phan <j.phan@newmotion.com>
2020-10-18 13:25:34 +0200

    start button

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -8,13 +8,15 @@ export default class MainMenu extends Phaser.Scene {
   }
 
   create() {
-    this.playButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2, 'button').setDepth(1);
+    this.playButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height * 2 / 3, 'button');
+    this.playButton.scale = 2;
     this.playButton.setInteractive();
+    const text = this.add.text(this.game.renderer.width / 2 - 45, this.game.renderer.height * 2 / 3 - 15, "START");
+    text.scale = 2;
 
     this.playButton.on('pointerup', () => {
       this.playButton = this.add
         .image(this.game.renderer.width / 2, this.game.renderer.height / 2, 'button-press')
-        .setDepth(1);
       this.scene.start('game');
     });
   }
